64fc440768b9de6266e5664784f64a239e1b0543
mauriciobau-maker <mauricio.bau@gmail.com>
2026-08-17 17:14:37 -0400

    feat(parry): expose isolated V0.1 module

---
+++ artifacts/api-server/src/parry/index.ts
@@ -0,0 +1,6 @@
+export * from "./parry.types";
+export * from "./parry.context";
+export * from "./parry.service";
+export * from "./parry.tool-registry";
+export * from "./providers/ai-provider";
+export * from "./tools/get-my-profile";
