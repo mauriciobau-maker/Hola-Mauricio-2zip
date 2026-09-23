8564acba3bd5240a67d253dc06c149d4611a02fa
mauriciobau-maker <mauricio.bau@gmail.com>
2026-09-22 23:48:41 -0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
-import { authMiddleware } from "./middlewares/authMiddleware";
+import { authMiddleware, clerkAuth } from "./middlewares/authMiddleware";
 
 const app: Express = express();
 
@@ -31,6 +31,7 @@ app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(clerkAuth);
 app.use(authMiddleware);
 
 app.use("/api", router);
